ea77d442ce24084f96083413aa15421d2161b131
kyrie-leon <gaomeng9518@163.com>
2021-06-29 21:40:13 +0800

    高并发内存池day1

---
+++ ConcurrentMemoryPool/ThreadCache.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include"Common.h"
+
+class ThreadCache
+{
+public:
+	//申请内存对象
+	void * Allocate(size_t size);
+
+	//释放内存对象
+	void* Deallocate(void* ptr, size_t size);
+
+	//从中心缓存获取对象
+	void* FetchFromCentralCache(size_t index, size_t size);
+
+	//释放对象后，链表过长，将内存回收到Central Cache
+	void ListTooLong(FreeList* list, size_t size);
+private:
+	FreeList _freeList[NFREELISTS];	//自由链表
+};
+
+//thread local storage(TLS)，每个进程有独立的地址空间，解决互斥锁问题
+static __declspec(thread) ThreadCache* tls_threadcache = nullptr;
