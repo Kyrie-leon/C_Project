f2f9887c5f9c519a5044cd088d0dd09942bbbb7a
kyrie-leon <gaomeng9518@163.com>
2021-07-22 21:37:58 +0800

    高并发内存池day4

--- ConcurrentMemoryPool/Test.cpp
+++ ConcurrentMemoryPool/Test.cpp
@@ -1,6 +1,14 @@
-#include"Common.h"
+#include"ConcurrentAlloc.h"
+
+void TestConcurrentAlloc()
+{
+	void* ptr1 = ConcurrentAlloc(8);
+	void* ptr2 = ConcurrentAlloc(16);
+
+}
 
 int main()
 {
+	TestConcurrentAlloc();
 	return 0;
 }
