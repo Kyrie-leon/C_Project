9d9ae1d51c9ee7c7e0cfb67a130934faaff16977
kyrie-leon <gaomeng9518@163.com>
2021-07-27 18:57:02 +0800

    PageHeap

--- ConcurrentMemoryPool/PageCache.h
+++ ConcurrentMemoryPool/PageCache.h
@@ -4,12 +4,33 @@
 class PageCache
 {
 public:
+	static PageCache* GetInstance()
+	{
+		return &_sInst;
+	}
 	//向系统申请k页内存挂到自由链表
 	void* SystemAllocPage(size_t k);
 	
+	//申请一个新的span
 	Span* NewSpan(size_t k);
+
+	// 获取从对象到span的映射
+	Span* MapObjectToSpan(void* obj);
+	// 释放空闲span回到Pagecache，并合并相邻的span
+	void ReleaseSpanToPageCahce(Span* span);
+
+
 private:
 	SpanList _spanList[NPAGES];	//按页数映射
-};
+	std::unordered_map<PageID, Span*> _idSpanMap;	//页号和内存地址的映射
 
-static PageCache pageCache;
+	std::recursive_mutex _mtx;
+
+private:
+	PageCache()
+	{}
+
+	PageCache(const PageCache&) = delete;
+	//单例
+	static PageCache _sInst;
+};
