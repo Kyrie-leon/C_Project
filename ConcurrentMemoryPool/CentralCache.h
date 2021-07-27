7c832274849a2ec546f7546562f54a927e92af22
kyrie-leon <gaomeng9518@163.com>
2021-07-27 18:55:30 +0800

    CentralCache

--- ConcurrentMemoryPool/CentralCache.h
+++ ConcurrentMemoryPool/CentralCache.h
@@ -4,6 +4,11 @@
 class CentralCache
 {
 public:
+	static CentralCache* GetInstance()
+	{
+		return &_sInst;
+	}
+
 	//从中心缓存获取一定数量的对象给thread cache
 	size_t FetchRangeObj(void*& start, void*& end, size_t n, size_t byte_size);
 
@@ -14,6 +19,11 @@ public:
 	void ReleaseListToSpans(void* start, size_t byte_size);
 private:
 	SpanList _spanLists[NLISTS];	//按对齐方式映射，把每一个span挂起来
-};
+private:
+	CentralCache()
+	{}
+	//C++98拷贝构造只声明不定义
+	CentralCache(const CentralCache&) = delete;
 
-static CentralCache centralCache;
+	static CentralCache _sInst;
+};
