f1d435ae1cd8dedd7744c02660fed9e6ea0e0bc7
kyrie-leon <gaomeng9518@163.com>
2021-07-27 18:56:16 +0800

    malloc/free接口文件

--- ConcurrentMemoryPool/ConcurrentAlloc.h
+++ ConcurrentMemoryPool/ConcurrentAlloc.h
@@ -2,16 +2,23 @@
 
 #include "Common.h"
 #include "ThreadCache.h"
+#include"PageCache.h"
 #include<assert.h>
 
 
 //为每个线程分配空间
-void* ConcurrentAlloc(size_t size)
+static void* ConcurrentAlloc(size_t size)
 {
 	//保证多线程安全，使用TLS技术，线程安全的
 	if (size > MAX_BYTES)
 	{
-		//PageCache
+		//PageCache,65  68,以页为单位
+		size_t npage = SizeClass::RoundUp(size) >> PAGE_SHIFT;	//以页为单位取整
+		Span* span = PageCache::GetInstance()->NewSpan(npage);
+		span->_objsize = size;
+
+		void* ptr = (void*)(span->_pageId << PAGE_SHIFT);
+		return ptr;
 	}
 	else
 	{
@@ -27,16 +34,20 @@ void* ConcurrentAlloc(size_t size)
 
 
 //释放线程空间
-void ConcurrentFree(void* ptr, size_t size)
+static void ConcurrentFree(void* ptr)
 {
-	assert(tls_threadcache);
+	//获取对象到span的映射
+	Span* span = PageCache::GetInstance()->MapObjectToSpan(ptr);
+	size_t size = span->_objsize;
 
 	if (size > MAX_BYTES)
 	{
 		// PageCache
+		PageCache::GetInstance()->ReleaseSpanToPageCahce(span);
 	}
 	else
 	{
+		assert(tls_threadcache);
 		tls_threadcache->Deallocate(ptr, size);
 	}
 }
