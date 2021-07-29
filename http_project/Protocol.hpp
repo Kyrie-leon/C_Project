4c4679eac1141b2b2c462a4a48c8f3aaa0de9147
kyrie-leon <gaomeng9518@163.com>
2021-07-29 16:23:21 +0800

    Http服务器

--- http_project/Protocol.hpp
+++ http_project/Protocol.hpp
@@ -1,1 +1,23 @@
 #pragma once
+#include"Sock.hpp"
+#include"Log.hpp"
+
+class Entry{
+  public:
+    //HandlerHttp处理函数
+    // 1.sock解引用
+    static void *HandlerHttp(void* arg)
+    {
+      int sock = *(int*)arg;
+      delete (int*)arg;
+//测试代码显示http请求
+#ifdef DEBUG
+    char request[10240];
+    recv(sock, request, sizeof(request), 0);
+    std::cout << request << std::endl;
+    close(sock);
+#else
+    std::cout<< "other code.." << std::endl;
+#endif
+    }
+};
